a0cf0bc6a28bafb82b16fd03962caac595ba83b8
Luca Lavezzo <llavez99@gmail.com>
2023-09-22 16:44:09 -0400

    added dyturbo and atlas to A_i

--- scripts/utilities/explore_angular_coefficients.py
+++ scripts/utilities/explore_angular_coefficients.py
@@ -13,6 +13,8 @@ from wremnants import plot_tools
 import matplotlib.pyplot as plt
 import mplhep as hep
 import pdb
+import argparse
+import uproot
 
 def load_helicity_moments_for_sample_from_file(sampleName = "ZmumuPostVFP", filePath = None):
     if not os.path.exists(filePath):
@@ -31,38 +33,83 @@ def make_Ais_for_observable(obs, h, rebin = None):
 
     return h_coeff
 
-def plot_hist(h, xlabel = "", ylabel = "", xrange = None, yrange = None, corr = 1):
-    fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (8, 8), sharex = True)
+def plot_hist(h, xlabel = None, ylabel = None, label=None, xrange = None, yrange = None, corr = 1, ax=None):
+
+    if ax is None:
+        fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (8, 8), sharex = True)
 
     xValues = h.axes.centers[0]
     yValues = h.values() * corr
 #    yVariances = h.variances()
-    plt.plot(xValues, yValues, marker = '.', linestyle = 'dotted')
+
+    ax.plot(xValues, yValues, marker = '.', linestyle = 'dotted', label=label)
 #    plt.errorbar(xValues, yValues, yerr=yVariances, marker = ".", linestyle = "solid")
 
-    ax.set_xlabel(xlabel, fontsize = 22)
-    ax.set_ylabel(ylabel, fontsize = 22)
+    if xlabel:
+        ax.set_xlabel(xlabel, fontsize = 22)
+    if ylabel:
+        ax.set_ylabel(ylabel, fontsize = 22)
     ax.set_xscale("log")
     if xrange is not None:
         ax.set_xlim(xrange)
     if yrange is not None:
         ax.set_ylim(yrange)
 
-    return fig
+    try:
+        return fig, ax
+    except NameError:
+        return ax
 
 def save_fig(fig, outname):
     fig.set_tight_layout(True)
     fig.savefig(outname)
     print(f"{outname} created")
 
-def plot_Ai_from_hist_for_observable(Ai, h, obs, outname):
+def get_dyturbo_Ai(Ai, dyturbo_fname):
+    file = uproot.open(dyturbo_fname)
+    h = file[f"wgt_a{Ai}_y_qt"].to_hist().project("xaxis")
+    hs = file['s_qt'].to_hist()
+    h = hh.divideHists(h, hs)
+    return h
+
+def get_atlas_Ai(Ai, atlas_dname):
+    # from https://www.hepdata.net/record/76986
+    fname = f"HEPData-ins1466778-v1-Table_{Ai+2}.root"
+    file = uproot.open(atlas_dname+"/"+fname)
+    h = file['Table '+str(Ai + 2)]['Hist1D_y1'].to_hist()
+    return h
+
+def plot_Ai_from_hist_for_observable(Ai, h, obs, outname, dyturbo_fname=None, atlas_dname=None):
     hToPlot = h[{"helicity" : complex(Ai)}]
     corr = 1
-    if Ai == 4 or Ai == 1 or Ai == 3: corr = -1
-    save_fig(plot_hist(hToPlot, xlabel = obs, ylabel = f"$A_{Ai}$", xrange = None, yrange = None, corr = corr), outname)
+    # if Ai == 4 or Ai == 1 or Ai == 3: corr = -1
+    fig, ax = plot_hist(hToPlot, xlabel = obs, ylabel = f"$A_{Ai}$", label="minlo", xrange = None, yrange = None, corr = corr)
+
+    if dyturbo_fname and os.path.exists(dyturbo_fname) and Ai != -1:
+        if obs != "$p_{T, V}$":
+            raise Exception("Dyturbo only implemented for pT")
+        h_dyturbo_Ai = get_dyturbo_Ai(Ai, dyturbo_fname)
+        ax = plot_hist(h_dyturbo_Ai, label="dyturbo", ax=ax)
+
+    if atlas_dname and os.path.isdir(atlas_dname) and Ai != -1:
+        if obs != "$p_{T, V}$":
+            raise Exception("ATLAS only implemented for pT")
+        h_atlas_Ai = get_atlas_Ai(Ai, atlas_dname)
+        ax = plot_hist(h_atlas_Ai, label="ATLAS", ax=ax)
+
+    ax.legend()
+    save_fig(fig, outname)
 
 def main():
-    filePath = "/eos/home-f/fvazzole/TheoryAgnostic/AngularCoefficients/testpT/w_z_gen_dists_NonClosureCorl.hdf5" #FIXME
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help = "Input file name")
+    parser.add_argument("-o", "--outdir", help = "Output directory")
+    parser.add_argument("-d", "--dyturbo", help = "Dyturbo file name", default=None, required=False)
+    parser.add_argument("-a", "--atlas", help = "ATLAS directory name", default=None, required=False)
+    args = parser.parse_args()
+
+    filePath = f"{args.file}"
 
     # Load the helicity moments
     minnloZHelMom = load_helicity_moments_for_sample_from_file(sampleName = "ZmumuPostVFP", filePath = filePath)
@@ -74,10 +121,12 @@ def main():
     minnloZcoeff_massVgen = make_Ais_for_observable("massVgen", minnloZHelMom)
 
 
+    if not os.path.isdir(args.outdir):
+        os.makedir(args.outdir)
 
     for i in range(-1,8):
-        plot_Ai_from_hist_for_observable(i, minnloZcoeff_ptVgen, "$p_{T, V}$", f"/eos/home-f/fvazzole/TheoryAgnostic/AngularCoefficients/A_{i}_vs_pT_minnlo.png")
-        plot_Ai_from_hist_for_observable(i, minnloZcoeff_massVgen, "$m_V$", f"/eos/home-f/fvazzole/TheoryAgnostic/AngularCoefficients/A_{i}_vs_mV_minnlo.png")
+        plot_Ai_from_hist_for_observable(i, minnloZcoeff_ptVgen, "$p_{T, V}$", f"{args.outdir}/A_{i}_vs_pT_minnlo.png", dyturbo_fname=args.dyturbo, atlas_dname=args.atlas)
+        plot_Ai_from_hist_for_observable(i, minnloZcoeff_massVgen, "$m_V$", f"{args.outdir}/A_{i}_vs_mV_minnlo.png")
 
 if __name__ == "__main__":
     main()
